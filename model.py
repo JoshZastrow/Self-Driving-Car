3080ab8668cc043649cff03b8a0b34cd48379645
JoshZastrow <j.a.zastrow.jr@gmail.com>
2017-10-05 12:34:15 -0400

    write feature generator from VGG16;

--- model.py
+++ model.py
@@ -1,16 +1,17 @@
 
-from data_utils import load_data_batch
+from data_utils import DataGenerator
 import matplotlib.pyplot as plt
 import numpy as np 
-from keras.layers import Dense, GlobalAveragePooling2D
-from keras.models import Model
-from keras.preprocessing.image import ImageDataGenerator
-from keras.applications.inception_v3 import InceptionV3
+from keras import optimizers
+from keras.layers import Dense, Flatten, Dropout
+from keras.models import Model, Sequential
+from keras.applications import VGG16
+import csv
 
 # Set a random seed so I can run the same code and get the same result
 np.random.seed(7)
-    
-def transfer_InceptionV3():
+        
+def feature_generator(batch=30, sample_size=None):
     """
     Model Architecture:
     _____________________________________________________________________________
@@ -21,56 +22,107 @@ def transfer_InceptionV3():
     _____________________________________________________________________________
     InceptionV3          (None, 13, 18, 2048)  896      input_6[0][0]
     (Tranfer Model)
-    _____________________________________________________________________________
-    globalavepooling2d_6 (None, 2048)          0        InceptionV3[0][0]   
-    (Pool)                 
-    _____________________________________________________________________________
-    dense_9              (None, 1024)          2098176  globalavepooling2d_6[0][0]
-    (Dense) 
-    _____________________________________________________________________________
-    dense_10             (None, 1)             1025     dense_9[0][0]      
-    (Dense)              
+             
     =============================================================================
-    Total params: 23,711,169
-    Trainable params: 2,099,201
-    Non-trainable params: 21,611,968
+    Total params: 21,611,968
     """
+    csv_file = '../Car/datasets/HMB_1/output/interpolated.csv'
+    img_folder = '../Car/datasets/HMB_1/output/'
     
-    # Create
-    base_model = InceptionV3(include_top=False, 
-                             weights='imagenet',
-                             input_tensor=None,
-                             input_shape=(480, 640, 3))
-    for layer in base_model.layers:
-        layer.trainable = False
+    # Get number of lines in the csv file (if not provided)
+    if not sample_size:
+        with open(csv_file,"r") as f:
+            reader = csv.reader(f,delimiter = ",")
+            data = list(reader)
+            sample_size = len(data)
+            
+    data = DataGenerator() 
+    data = data.from_csv(csv_path=csv_file,
+                         img_dir=img_folder,
+                         batch_size=32)
+    
+    model = VGG16(include_top=False, weights='imagenet', )
+    
+    i = 0
+    
+    # Create empty array
+    features = np.zeros(shape=(sample_size, 15, 20 ,512))
+    labels = np.zeros(shape=(sample_size))
+    
+    for inputs, labels in data:
+        feature_batch = model.predict(inputs)
+        features[i : i + batch - 1] = feature_batch
+        labels[i : i + batch - 1] = labels
+        i += batch
+        if i >= sample_size: break
+
+    return features
+    
+
+class TransferModel():
+    
+    def __init__(self):
         
-    # Add top layers
-    top_layers = base_model.output
-    top_layers = GlobalAveragePooling2D()(top_layers)
-    top_layers = Dense(1024, activation='relu')(top_layers)
-    prediction = Dense(1)(top_layers)
-   
-    model = Model(input=base_model.input,
-                  output=prediction)    
-    
-    # Compile: Regression 
-    model.compile(optimizer='rmsprop', loss='mean_squared_error')
-    
-    return model 
+        # TODO: Add Conv base 
+        self.model = Sequential()
+        
+        self.model.add(Dense(1024, 
+                             activation='relu', 
+                             input_shape=(30, 13, 18, 2048)))
+        
+        self.model.add(Dropout(0.5))
+        
+        self.model.add(Dense(1))
+        
+        self.model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+                           loss='mean_squared_error',
+                           metrics=['accuracy'])
+        
+        
+        
+    def train(self, csv_file=None, img_dir=None):
+        
+        if not (csv_file or img_dir):
+            csv_file = '../Car/datasets/HMB_1/output/interpolated.csv'
+            img_folder = '../Car/datasets/HMB_1/output/'
+            
+        data_flow = DataGenerator()
+        data_flow = data_flow.from_csv(csv_path=csv_file,
+                                       img_dir=img_folder, 
+                                       batch_size=30)
+        return self.model.fit_generator(
+               data_flow,
+               samples_per_epoch=200,
+               nb_epoch=5,
+               verbose=2,
+               nb_val_samples=100,
+               validation_data=data_flow)
+                              
         
 if __name__ == "__main__":
-
-    model = transfer_InceptionV3()
     
-    # Load Data
-    data = load_udacity_data  (val_percent=0)
-    
-    model.fit(x=data['X_train'], 
-              y=data['Y_train'], 
-              validation_split=0.2,
-              verbose=2,
-              nb_epoch=3)
-    
-    score = model.evaluate(x=data['X_valid'], y=data['Y_valid'])
-    print(score)
-    
+    features = feature_generator()
+    print('feature size:', features.shape)
+#    tfmodel = TransferModel()
+#    
+#    history = tfmodel.train()
+#    
+#    acc = history.history['acc']
+#    val_acc = history.history['val_acc']
+#    loss = history.history['loss']
+#    val_loss = history.history['val_loss']
+#    
+#    epochs = range(len(acc))
+#    
+#    plt.plot(epochs, acc, 'bo', label='Training Accuracy')
+#    plt.plot(epochs, val_acc, 'b', label='Validation Accuracy')
+#    plt.title('Training and Validation accuracy')
+#    plt.legend()
+#    plt.figure()
+#    
+#    plt.plot(epochs, loss, 'bo', label='Training loss')
+#    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+#    plt.title('Training and Validation loss')
+#    plt.legend()
+#    
+#    plt.show()
