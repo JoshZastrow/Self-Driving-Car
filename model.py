6ce4599561da5bc1a1a90e151c29805af00cb7a4
JoshZastrow <j.a.zastrow.jr@gmail.com>
2017-10-09 13:03:04 -0400

    start writing a data writer to py tables

--- model.py
+++ model.py
@@ -1,33 +1,35 @@
 
 from data_utils import DataGenerator
-import matplotlib.pyplot as plt
 import numpy as np 
+import pandas as pd
 from keras import optimizers
-from keras.layers import Dense, Flatten, Dropout
-from keras.models import Model, Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.models import Sequential
 from keras.applications import VGG16
 import csv
+from tables import open_file, Atom
 
 # Set a random seed so I can run the same code and get the same result
 np.random.seed(7)
         
-def feature_generator(batch=30, sample_size=None):
+def feature_generator(batch=30, sample_size=None, 
+                      csv_file=None, img_folder=None):
     """
-    Model Architecture:
-    _____________________________________________________________________________
-    Layer (type)         Output Shape          Param #  Connected to                     
-    =============================================================================
-    input_6              (None, 480, 640, 3)   0                                            
-    (InputLayer)
-    _____________________________________________________________________________
-    InceptionV3          (None, 13, 18, 2048)  896      input_6[0][0]
-    (Tranfer Model)
-             
-    =============================================================================
-    Total params: 21,611,968
+    Convolutional Base Model Architecture:
+ ____________________________________________________________________________________________________
+Layer (type)                     Output Shape          Param #     Connected to                     
+====================================================================================================
+vgg16 (Model)                    (None, 15, 20, 512)   14714688                                     
+____________________________________________________________________________________________________
+flatten_5 (Flatten)              (None, 153600)        0           block5_pool[0][0]                
+====================================================================================================
+    Total params: 14,714,688
+    Trainable params: 14,714,688
+    Non-trainable params: 0
     """
-    csv_file = '../Car/datasets/HMB_1/output/interpolated.csv'
-    img_folder = '../Car/datasets/HMB_1/output/'
+    if not csv_file:
+        csv_file = '../Car/datasets/HMB_1/output/interpolated.csv'
+        img_folder = '../Car/datasets/HMB_1/output/'
     
     # Get number of lines in the csv file (if not provided)
     if not sample_size:
@@ -36,27 +38,30 @@ def feature_generator(batch=30, sample_size=None):
             data = list(reader)
             sample_size = len(data)
             
+    print('initializing data generator...')
     data = DataGenerator() 
     data = data.from_csv(csv_path=csv_file,
                          img_dir=img_folder,
-                         batch_size=32)
-    
-    model = VGG16(include_top=False, weights='imagenet', )
+                         batch_size=batch)
+    print('initializing model....')
+    conv_base = VGG16(include_top=False, 
+                      weights='imagenet', 
+                      input_shape=(480, 640, 3))
+
+    model = Sequential()
+    model.add(conv_base)
+    model.add(Flatten())
     
     i = 0
     
-    # Create empty array
-    features = np.zeros(shape=(sample_size, 15, 20 ,512))
-    labels = np.zeros(shape=(sample_size))
-    
-    for inputs, labels in data:
+    print('Performing Feature Extraction:', end='')
+    for inputs, y in data:
         feature_batch = model.predict(inputs)
-        features[i : i + batch - 1] = feature_batch
-        labels[i : i + batch - 1] = labels
+        print('batch {} through {} complete'.format(i, i + batch))
+        yield feature_batch, y
+        
         i += batch
         if i >= sample_size: break
-
-    return features
     
 
 class TransferModel():
@@ -68,7 +73,7 @@ class TransferModel():
         
         self.model.add(Dense(1024, 
                              activation='relu', 
-                             input_shape=(30, 13, 18, 2048)))
+                             input_shape=(5, 13, 18, 2048)))
         
         self.model.add(Dropout(0.5))
         
@@ -77,8 +82,6 @@ class TransferModel():
         self.model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
                            loss='mean_squared_error',
                            metrics=['accuracy'])
-        
-        
         
     def train(self, csv_file=None, img_dir=None):
         
@@ -89,7 +92,7 @@ class TransferModel():
         data_flow = DataGenerator()
         data_flow = data_flow.from_csv(csv_path=csv_file,
                                        img_dir=img_folder, 
-                                       batch_size=30)
+                                       batch_size=5)
         return self.model.fit_generator(
                data_flow,
                samples_per_epoch=200,
@@ -97,14 +100,84 @@ class TransferModel():
                verbose=2,
                nb_val_samples=100,
                validation_data=data_flow)
-                              
         
+                
+class array_writer():
+    """
+    Constructs the pytable storage container for the data, then on 
+    each call stores the data in an array
+    """
+    def __init__(self, chunksize, dataset='HMB_1'):
+        self.fileh = open_file('VGG16/{}.h5'.format(dataset), mode = 'w')
+        self.root = self.fileh.root
+        self.c = chunksize
+        
+        if dataset not in self.root:
+            
+            self.HMB1_group = self.fileh.create_group(self.root, dataset)
+        
+            # feature_table = self.fileh.create_table(HMB1_group, 'features')
+            # label_table = self.fileh.create_table(HMB1_group, 'labels')
+            
+    def __call__(self, arr1, arr2):
+        
+        a1 = Atom.from_dtype(arr1)
+        a2 = Atom.from_dtype(arr2)
+            
+        if 'test_data1' not in self.HMB1_group:
+            self.test_array1 = self.fileh.create_earray(self.HMB1_group,
+                                         'test_data1', a1,
+                                         shape=(0, len(arr1)),
+                                         title='Features Tester',
+                                         expectedrows=20,
+                                         chunkshape=(self.c, len(arr1)))
+    
+            self.test_array2 = self.fileh.create_earray(self.HMB1_group,
+                                         'test_data2', a2,
+                                         shape=(0, len(arr2)),
+                                         title='Features Tester',
+                                         expectedrows=20,
+                                         chunkshape=(self.c, len(arr2)))
+        else:
+           self.test_array1 = self.root.HMB1_group.test_data1
+           self.test_array2 = self.root.HMB1_group.test_data2
+            
+        self.test_array1.append(arr1)
+        self.test_array2.append(arr2)
+         
+
+def setup_table(f):
+    with fileh as  open_file('VGG16/{}.h5'.format(dataset), mode = 'w')
 if __name__ == "__main__":
     
-    features = feature_generator()
-    print('feature size:', features.shape)
-#    tfmodel = TransferModel()
+    batch_size = 2
+    # Create empty array
+    # features = np.zeros(shape=(sample_size, 153600))
+    # labels = np.zeros(shape=(sample_size))
+    
+    VGG = feature_generator(batch=batch_size, sample_size=10)
+    
+    i = 0
+    
+    # Store "x" in a chunked array...
+    # f = tables.openFile('VGG16/HMB_1.h5', 'w')
+    writer = array_writer(batch_size)
+#    for feature, label in VGG:
+#        writer(feature, label)
+        # np.savetxt('features.csv', feature)
+#        if 'features' in store:
+#            
+#            dfx.to_hdf('VGG16/HMB_1.h5', 'features', append=True)
+#            dfy.to_hdf('VGG16/HMB_1.h5', 'labels', append=True)
 #    
+#        else:
+#            store.append('features', dfx)
+#            store.append('labels', dfy)
+#        print('data stored')
+    # dfx.to_csv(outfile_features)
+    # dfy.to_csv(outfile_labels)
+    
+#    tfmodel = TransferModel()
 #    history = tfmodel.train()
 #    
 #    acc = history.history['acc']
