12f2028612779bfbac86f5ebf1588654bb6f6621
JoshZastrow <j.a.zastrow.jr@gmail.com>
2017-10-20 11:27:44 -0400

    Switch from VGG to InceptionV3

--- model.py
+++ model.py
@@ -1,14 +1,14 @@
 
-from data_utils import DataGenerator
+from data_utils import DataGenerator, DataWriter
 import numpy as np 
 import pandas as pd
 from keras import optimizers
 from keras.layers import Dense, Dropout, Flatten, MaxPooling2D
 from keras.models import Sequential
-from keras.applications import VGG16
+from keras.applications import VGG16, InceptionV3
 import csv
-from tables import open_file, Atom
 import time
+from tables import open_file
 
 # Set a random seed so I can run the same code and get the same result
 np.random.seed(7)
@@ -17,13 +17,13 @@ def feature_generator(batch=30, sample_size=None,
                       csv_file=None, img_folder=None):
     """
     Convolutional Base Model Architecture:
- ____________________________________________________________________________________________________
+ ______________________________________________________________________________
 Layer (type)                     Output Shape          Param #     Connected to                     
-====================================================================================================
+===============================================================================
 vgg16 (Model)                    (None, 15, 20, 512)   14714688                                     
-____________________________________________________________________________________________________
+________________________________________________________________________________
 flatten_5 (Flatten)              (None, 153600)        0           block5_pool[0][0]                
-====================================================================================================
+===============================================================================
     Total params: 14,714,688
     Trainable params: 14,714,688
     Non-trainable params: 0
@@ -46,19 +46,18 @@ flatten_5 (Flatten)              (None, 153600)        0           block5_pool[0
                          batch_size=batch)
     
     t1 = time.time()
-    print('{}seconds\ninitializing model... '
+    print('{} seconds\ninitializing model... '
           .format(round(t1 - t0, 2)), end='')
     
-    conv_base = VGG16(include_top=False, 
-                      weights='imagenet', 
-                      input_shape=(480, 640, 3))
+    conv_base = InceptionV3(include_top=False, 
+                            weights='imagenet', 
+                            input_shape=(480, 640, 3))
     
     model = Sequential()
     model.add(conv_base)
-    # model.add(MaxPooling2D(pool_size=(3, 4)))
+    model.add(MaxPooling2D(pool_size=(3, 4)))
     model.add(Flatten())
     
-
     i = 0
     t2 = time.time()
     
@@ -68,6 +67,7 @@ flatten_5 (Flatten)              (None, 153600)        0           block5_pool[0
     print('-------------')
     print(model.summary())
     print('\nPerforming Feature Extraction:\n')
+    
     for inputs, y in data:
         cur = time.time()
         feature_batch = model.predict(inputs)
@@ -117,68 +117,21 @@ class TransferModel():
                verbose=2,
                nb_val_samples=100,
                validation_data=data_flow)
-         
-
-class array_writer(object):
-    """
-    writes numpy array to .h5 file. Creates a dataset group, then 
-    a feature and label subgroup which stores each array named with their
-    original filename.
-    """
-    def __init__(self, fileh, dataset, sample):
-        root = fileh.root
-        a1 = Atom.from_dtype(sample[0].dtype)
-        a2 = Atom.from_dtype(sample[1].dtype)
-        batch, size = sample[0].shape
-        
-        if dataset not in fileh.root:
-            group = fileh.create_group(
-                    root, 
-                    name=dataset,
-                    title='{} dataset'.format(dataset))
-        else:
-            group = fileh.get_node(root, dataset)
-            
-        if 'features' not in group:
-            self.features = fileh.create_earray(
-                    where=group,
-                    name='features',
-                    atom=a1,
-                    shape=(0, size),
-                    title='feature dataset', 
-                    chunkshape=(batch, size))
-        else:
-            self.features = fileh.get_node(group, 'features')
-            
-        if 'labels' not in group:
-            self.labels = fileh.create_earray(
-                    where=group,
-                    name='labels',
-                    atom=a2,
-                    shape=(0,),
-                    title='label dataset',
-                    chunkshape=(batch,))                        
-        else:
-            self.labels = fileh.get_node(group, 'labels')
-            
-    def __call__(self, x, y):
-        self.features.append(x)
-        self.labels.append(y)
             
         
 if __name__ == "__main__":
     
     t0 = time.time()
     
-    VGG = feature_generator(batch=1, sample_size=3)
+    Inception = feature_generator(batch=5, sample_size=15)
     
-    with open_file('VGG16/VGG16.h5', mode = 'w') as h5:
+    with open_file('Inception/InceptionV3.h5', mode = 'w') as h5:
 
         i = 0
-        for x, y in VGG:
+        for x, y in Inception:
             if i > 4: break
             if i == 0:
-                w = array_writer(h5, dataset='Test', sample=(x, y))
+                w = DataWriter(h5, dataset='Test', sample=(x, y))
             w(x, y)
             
             i += 1
