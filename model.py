cf51d9bdb3bd944bfb19d3ad5dfa9bd20ba6c90d
Josh Zastrow <j.a.zastrow.jr@gmail.com>
2017-10-29 13:22:12 +0100

    add argparser for command line input

--- model.py
+++ model.py
@@ -1,79 +1,90 @@
-
 from data_utils import DataGenerator, DataWriter
 import numpy as np 
-import pandas as pd
+import argparse
 from keras import optimizers
 from keras.layers import Dense, Dropout, Flatten, MaxPooling2D
 from keras.models import Sequential
-from keras.applications import VGG16, InceptionV3
+from keras.applications import InceptionV3, VGG16, ResNet50, Xception
 import csv
-import time
 from tables import open_file
+import os
 
-# Set a random seed so I can run the same code and get the same result
+# If I go for random sampling... which I don't at the moment.
 np.random.seed(7)
         
 def feature_generator(batch=30, sample_size=None, 
-                      csv_file=None, img_folder=None):
+                      csv_file=None, img_folder=None,
+                      model='InceptionV3'):
     """
-    Convolutional Base Model Architecture:
- ______________________________________________________________________________
-Layer (type)                     Output Shape          Param #     Connected to                     
-===============================================================================
-vgg16 (Model)                    (None, 15, 20, 512)   14714688                                     
-________________________________________________________________________________
-flatten_5 (Flatten)              (None, 153600)        0           block5_pool[0][0]                
-===============================================================================
-    Total params: 14,714,688
-    Trainable params: 14,714,688
-    Non-trainable params: 0
+    Transfer Learning function.
+    Convolutional Base Model Architecture, top layers (classifier) removed
+    for low level feature genaration from raw data. Model pulls from a dataset
+    and executes a prediction function to generate extracted features.
+    
+    inputs
+    ------
+    batch: <int> number of samples to run through the model during prediction
+    sample_size: <int> total number of samples to use.
+    csv_file: <str> file path to log file. Udacity's file is interpolated.csv
+    img_folder: <str> directory path to images. Udacity has three sub folders of images
+    model: <str> name of model you would like to use for feature extraction. 
+    
     """
+    
+    # for when I run locally... should be deleted..
     if not csv_file:
         csv_file = '../Car/datasets/HMB_1/output/interpolated.csv'
         img_folder = '../Car/datasets/HMB_1/output/'
     
     # Get number of lines in the csv file (if not provided)
     if not sample_size:
-        with open(csv_file,"r") as f:
-            reader = csv.reader(f,delimiter = ",")
-            data = list(reader)
-            sample_size = len(data)
+    with open(csv_file,"r") as f:
+        reader = csv.reader(f,delimiter = ",")
+        data = list(reader)
+        rows = len(data)
+    
+    if not sample_size or sample_size > rows:
+        sample_size = rows
             
     print('initializing data generator...', end='')
     data = DataGenerator() 
     data = data.from_csv(csv_path=csv_file,
                          img_dir=img_folder,
                          batch_size=batch)
-    
-    t1 = time.time()
-    print('{} seconds\ninitializing model... '
-          .format(round(t1 - t0, 2)), end='')
-    
-    conv_base = InceptionV3(include_top=False, 
-                            weights='imagenet', 
-                            input_shape=(480, 640, 3))
-    
+
+    if model == 'InceptionV3':
+        conv_base = InceptionV3(include_top=False, 
+                                weights='imagenet', 
+                                input_shape=(480, 640, 3))
+    elif model == 'VGG16':
+        conv_base = VGG16(include_top=False, 
+                                weights='imagenet', 
+                                input_shape=(480, 640, 3)) 
+    elif model == 'ResNet50':
+        conv_base = ResNet50(include_top=False, 
+                                weights='imagenet', 
+                                input_shape=(480, 640, 3))        
+    elif model == 'Xception':
+        conv_base = Xception(include_top=False, 
+                                weights='imagenet', 
+                                input_shape=(480, 640, 3))    
+    else:
+        model == 'InceptionV3'
+        
     model = Sequential()
     model.add(conv_base)
-    model.add(MaxPooling2D(pool_size=(3, 4)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     
     i = 0
-    t2 = time.time()
     
-    print('{} seconds\n'
-          .format(round(t2 - t1, 2)))
-    print('\nModel Summary')
-    print('-------------')
-    print(model.summary())
     print('\nPerforming Feature Extraction:\n')
     
     for inputs, y in data:
-        cur = time.time()
         feature_batch = model.predict(inputs)
-        new = time.time()
-        print('\tbatch {} through {} complete ({} sec)'
-              .format(i, i + batch, round(new - cur, 2)))
+
+        print('\tbatch {} through {} complete'
+              .format(i, i + batch))
 
         yield feature_batch, y
         
@@ -81,8 +92,11 @@ flatten_5 (Flatten)              (None, 153600)        0           block5_pool[0
         if i >= sample_size: break
     
 
-class TransferModel():
-    
+class RegressionModel():
+    """
+    A basic densely connected Neural Network for regression on extracted features
+    from earlier feature extraction models.
+    """
     def __init__(self):
         
         # TODO: Add Conv base 
@@ -121,19 +135,60 @@ class TransferModel():
         
 if __name__ == "__main__":
     
-    t0 = time.time()
+    parser = argparse.ArgumentParser(description='Executes a '
+                                                 'state of the art ML model '
+                                                 'for feature extraction')
+    parser.add_argument('-log', metavar='csv_file', type=str, nargs='?',
+                        required=True,
+                        help='A file path to the CSV log file holding the '
+                             'sensor data.\n Example: data/interpolated.csv')
+    parser.add_argument('-img_dir', metavar='image_folder', type=str, nargs='?',
+                        required=True,
+                        help='path to the directory containing the image folders'
+                             '\n\nNOTE: This directory should have the center,'
+                             ' left, and right image subfolders as referenced'
+                             ' in the log file. This directory will be the'
+                             ' base path for the image file paths listed'
+                             ' in the log file. Example: data/images')
+    parser.add_argument('-model', type=str, nargs='?',
+                        help='the model you would like to use for feature'
+                             ' extraction. Current options are:'
+                             '\t-InceptionV3\n'
+                             '\t-VGG16\n'
+                             '\t-ResNet50\n'
+                             '\t-Xception'
+                             '\n\nleaving this blank will default to InceptionV3')
+    parser.add_argument('-output', type=str, nargs='?',
+                        required=True,
+                        help='Output file path. Must be an .h5 file'
+                             '\n\nExample:\n\t'
+                             'outputs/InceptionV3.h5')
     
-    Inception = feature_generator(batch=5, sample_size=15)
+    args = parser.parse_args()
     
-    with open_file('Inception/InceptionV3.h5', mode = 'w') as h5:
+    assert os.path.isfile(args.log), 'Cannot find log file from input path'
+    assert os.path.isfile(args.output), 'output file does not exist'
+    assert os.path.isdir(args.img_dir), 'image diretory cannot be found'
+    assert args.model in ['InceptionV3', 'VGG16'], 'input model is not an option'
+    
+    # Pulls data from the dataset and runs a prediction
+    # The prediction is a semi processed batch of data.
+    # the output of this processing is paired with the ground truth of the data.
+    model = feature_generator(batch=5, sample_size=10,
+                              csv_file=args.log, 
+                              img_folder=args.img_dir,
+                              model=args.model)
 
-        i = 0
-        for x, y in Inception:
-            if i > 4: break
-            if i == 0:
-                w = DataWriter(h5, dataset='Test', sample=(x, y))
-            w(x, y)
-            
-            i += 1
+    with open_file(args.output, mode = 'w') as h5:
+        first = True
+        
+        for output, y in model:
+            if first:
+                name = os.path.basename(args.output)
+                name = os.splitext(name)[0]
+                store = DataWriter(h5, dataset=name, sample=(output, y))
+
+            store(output, y)
+            first = False
 
     
